34f64a0d230af4e891dc8cb7ed6b656bc4850d9a
Thomas Weise <tweise@hfuu.edu.cn>
2024-06-13 06:21:31 +0800

    fix for UP017 ruff issue 2

--- tests/pycommons/dev/doc/test_setup_doc.py
+++ tests/pycommons/dev/doc/test_setup_doc.py
@@ -44,7 +44,7 @@ def test_setup_doc() -> None:
     else:
         pytest.fail("There should be an error!")
 
-    current_year: Final[int] = datetime.now(timezone.utc).year
+    current_year: Final[int] = datetime.now(timezone.utc).year  # noqa
 
     with temp_dir() as td:
         setup_doc(td, prj_base, current_year, None, None, None, None)
